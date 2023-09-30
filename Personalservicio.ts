d6fd3fc89726a882bcc3d0f7f55d4c9d0c585a3b
alaynhoppe <145080741+alaynhoppe@users.noreply.github.com>
2023-09-29 20:30:53 -0500

    Update Personalservicio.ts

--- Personalservicio.ts
+++ Personalservicio.ts
@@ -1,31 +1,21 @@
 import { Empleado } from "./Empleados";
-
-// La clase Personal de Servicio es una subclase de Empleado que hereda los datos de Persona y Empleado
 export class PersonalDeServicio extends Empleado {
-  private _seccion: string;
+  private seccion: string;
 
   constructor(
-    nombre: string,
-    apellido: string,
-    ci: number,
-    estadoCivil: string,
-    anioIncorporacion: number,
-    despacho: number,
-    seccion: string
-  ) {
-    super(nombre, apellido, ci, estadoCivil, anioIncorporacion, despacho);
-    this._seccion = seccion;
+    nombre: string,apellido: string,cedula: number,estadoCivil: string,Incorporacion: number,despacho: number,
+    seccion: string) {
+    super(nombre, apellido, cedula, estadoCivil, Incorporacion, despacho);
+    this.seccion = seccion;
   }
-
-  public get seccion(): string {
+  public get _seccion(): string {
     return this._seccion;
   }
 
   public cambiarSeccion(seccion: string) {
-    this._seccion = seccion;
+    this.seccion = seccion;
   }
-
-  toString(): string {
-    return `El personal de servicio ${this.nombre} ${this.apellido} de cédula ${this.ci} se encuentra en la sección de ${this.seccion}`;
+  mostrar(): string {
+    return `El personal de servicio se encuentra en la sección de ${this.seccion}`;
   }
 }
